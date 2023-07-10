236269142f97565520e7586f9d02211df4dac4b0
zzeis <moisesmonteiro.stark@gmail.com>
2023-07-10 14:34:45 -0300

    Replacing the information with generic examples

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -17,20 +17,7 @@ class Home extends BaseController
             return redirect()->to('/closed'); // Redireciona para a página de fechamento
         }
     }
-    public function teste222()
-    {
-        $currentDate = date('Y-m-d'); // Obtém a data atual no formato "ano-mês-dia"
-
-        // Verifica se a data atual é menor ou igual a 07/07/2023
-        if ($currentDate <= '2023-07-07') {
-            echo view('header/index');
-            echo view('home/index');
-            echo view('footer/index');
-        } else {
-            return redirect()->to('/closed'); // Redireciona para a página de fechamento
-        }
-    }
-
+    
     public function closed()
     {
         echo view('header/index');
