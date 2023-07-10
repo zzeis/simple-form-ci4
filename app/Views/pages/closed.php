2c55a8482d87590fe1799a786cb8fd9c382237c6
zzeis <moisesmonteiro.stark@gmail.com>
2023-07-10 14:35:24 -0300

    Replacing the information with generic example

--- app/Views/pages/closed.php
+++ app/Views/pages/closed.php
@@ -7,8 +7,8 @@
 <div class="home">
     <div class="text-center">
         <p class="icon"><i class="ri-error-warning-fill"></i></p>
-        <p>Inscrições encerradas!</p>
-        <p>Para mais informações acesse:</p>
-        <p><a href="https://www.iguape.sp.gov.br/site/">www.iguape.sp.gov.br</a></p>
+        <p>closed</p>
+        <p>info example</p>
+        <p><a href="#"></a></p>
     </div>
 </div>
